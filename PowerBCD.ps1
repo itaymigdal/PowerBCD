ab56661b163891c2ceea271e101d40ee9aab56dc
Itay Migdal <32266629+itaymigdal@users.noreply.github.com>
2025-01-06 19:47:00 +0200

    works without system privs

--- PowerBCD.ps1
+++ PowerBCD.ps1
@@ -1,6 +1,9 @@
-$REG_BASE_PATH = "HKLM:\BCD00000000\Objects"
+#Requires -RunAsAdministrator
+
+# Constants
+$REG_BASE_PATH = "HKLM:\BCD00000000"
 $GUID_BOOT_MANAGER = "{9dea862c-5cdd-4e70-acc1-f32b344d4795}"
-$GUID_BOOT_LOADER_PATH = "$REG_BASE_PATH\$GUID_BOOT_MANAGER\Elements\23000003"
+$GUID_BOOT_LOADER_PATH = "$REG_BASE_PATH\Objects\$GUID_BOOT_MANAGER\Elements\23000003"
 $ELEMENT_RECOVERYENABLED = "16000009"
 $ELEMENTVALUE_RECOVERYENABLED_NO = [byte]0
 $ELEMENTVALUE_RECOVERYENABLED_YES = [byte]1
@@ -14,7 +17,8 @@ $ELEMENT_TESTSIGNING = "16000049"
 $ELEMENTVALUE_TESTSIGNING_OFF = [byte]0
 $ELEMENTVALUE_TESTSIGNING_ON = [byte]1
 
-function Set-BCD {
+
+function PowerBCD {
     param (
         [ValidateSet("On", "Off")][string]$TestSigning,
         [ValidateSet("Yes", "No")][string]$RecoveryEnabled,
@@ -23,39 +27,68 @@ function Set-BCD {
     )
 
     try {
+       
+        # Permit ACL
+        $RegKey = [Microsoft.Win32.Registry]::LocalMachine.OpenSubKey(
+            $REG_BASE_PATH.Replace("HKLM:\", ""),
+            [Microsoft.Win32.RegistryKeyPermissionCheck]::ReadWriteSubTree,
+            [System.Security.AccessControl.RegistryRights]::ChangePermissions
+        )
+        $OriginalAcl = $RegKey.GetAccessControl()
+        $Acl = $OriginalAcl
+        $AdminRule = New-Object System.Security.AccessControl.RegistryAccessRule(
+            "Administrators",                     # User or Group
+            "FullControl",                        # Permissions
+            "ContainerInherit,ObjectInherit",     # Inheritance settings
+            "None",                               # Propagation settings
+            "Allow"                               # Access type
+        )
+        $Acl.SetAccessRule($AdminRule)
+        $RegKey.SetAccessControl($Acl)
+       
+        # Get active Boot Loader GID
         $ActiveLoaderGuid =  Get-ItemProperty -Path $GUID_BOOT_LOADER_PATH -Name Element | Select-Object -ExpandProperty Element
+       
+        # Change
         if ($PSBoundParameters.ContainsKey('TestSigning')) {
-            $elementKeyPath = "$REG_BASE_PATH\$ActiveLoaderGuid\Elements\$ELEMENT_TESTSIGNING"
+            $elementKeyPath = "$REG_BASE_PATH\Objects\$ActiveLoaderGuid\Elements\$ELEMENT_TESTSIGNING"
             $data = if ($TestSigning -eq "On") { $ELEMENTVALUE_TESTSIGNING_ON } else { $ELEMENTVALUE_TESTSIGNING_OFF }
             New-Item -Path $elementKeyPath -Force | Out-Null
             New-ItemProperty -Path $elementKeyPath -Name Element -PropertyType Binary -Value $data -Force | Out-Null
         }
-        elseif ($PSBoundParameters.ContainsKey('RecoveryEnabled')) {
-            $elementKeyPath = "$REG_BASE_PATH\$ActiveLoaderGuid\Elements\$ELEMENT_RECOVERYENABLED"
+        if ($PSBoundParameters.ContainsKey('RecoveryEnabled')) {
+            $elementKeyPath = "$REG_BASE_PATH\Objects\$ActiveLoaderGuid\Elements\$ELEMENT_RECOVERYENABLED"
             $data = if ($RecoveryEnabled -eq "Yes") { $ELEMENTVALUE_RECOVERYENABLED_YES } else { $ELEMENTVALUE_RECOVERYENABLED_NO }
             New-Item -Path $elementKeyPath -Force | Out-Null
             New-ItemProperty -Path $elementKeyPath -Name Element -PropertyType Binary -Value $data -Force | Out-Null
         }
-        elseif ($PSBoundParameters.ContainsKey('BootStatusPolicy')) {
-            $elementKeyPath = "$REG_BASE_PATH\$ActiveLoaderGuid\Elements\$ELEMENT_BOOTSTATUSPOLICY"
+        if ($PSBoundParameters.ContainsKey('BootStatusPolicy')) {
+            $elementKeyPath = "$REG_BASE_PATH\Objects\$ActiveLoaderGuid\Elements\$ELEMENT_BOOTSTATUSPOLICY"
             $data = if ($BootStatusPolicy -eq "DisplayAllFailures") { $ELEMENTVALUE_BOOTSTATUSPOLICY_DISPLAYALLFAILURES } else { $ELEMENTVALUE_BOOTSTATUSPOLICY_IGNOREALLFAILURES }
             New-Item -Path $elementKeyPath -Force | Out-Null
             New-ItemProperty -Path $elementKeyPath -Name Element -PropertyType Binary -Value $data -Force | Out-Null
         }
-        elseif ($PSBoundParameters.ContainsKey('SafeBoot')) {
-            $elementKeyPath = "$REG_BASE_PATH\$ActiveLoaderGuid\Elements\$ELEMENT_SAFEBOOT"
-            $data = if ($SafeBoot -eq "Minimal") { $ELEMENTVALUE_SAFEBOOT_MINIMAL } 
-                elseif ($SafeBoot -eq "Network") { $ELEMENTVALUE_SAFEBOOT_NETWORK }
-            New-Item -Path $elementKeyPath -Force | Out-Null
-            New-ItemProperty -Path $elementKeyPath -Name Element -PropertyType Binary -Value $data -Force | Out-Null
-            if ($SafeBoot -eq "Off")
-            {
+        if ($PSBoundParameters.ContainsKey('SafeBoot')) {
+            $elementKeyPath = "$REG_BASE_PATH\Objects\$ActiveLoaderGuid\Elements\$ELEMENT_SAFEBOOT"
+            if ($SafeBoot -eq "Off") {
                 Remove-ItemProperty -Path $elementKeyPath -Name Element -Force
                 Remove-Item -Path $elementKeyPath -Force
+            } 
+            else {
+                $data = if ($SafeBoot -eq "Minimal") { $ELEMENTVALUE_SAFEBOOT_MINIMAL }
+                    elseif ($SafeBoot -eq "Network") { $ELEMENTVALUE_SAFEBOOT_NETWORK }
+                New-Item -Path $elementKeyPath -Force | Out-Null
+                New-ItemProperty -Path $elementKeyPath -Name Element -PropertyType Binary -Value $data -Force | Out-Null
             }
         }
 
     } catch {
         Write-Error "An error occurred: $_"
+    }
+
+    finally {
+        # Restore ACL
+        $RegKey.SetAccessControl($OriginalAcl)
+        $RegKey.Close()
     }
 }
